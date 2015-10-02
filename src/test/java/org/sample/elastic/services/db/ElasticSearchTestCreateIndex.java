3d88b8d0681df05de831887bf30dd41f14aea993
rajanm <rajan.manickavasagam@gmail.com>
2015-10-02 19:15:29 +0530

    cleaned up

--- src/test/java/org/sample/elastic/services/db/ElasticSearchTestCreateIndex.java
+++ src/test/java/org/sample/elastic/services/db/ElasticSearchTestCreateIndex.java
@@ -9,8 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Date;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class ElasticSearchTestCreateIndex {
 
     private ElasticSampleConfiguration esConfig;
@@ -39,7 +37,7 @@ public class ElasticSearchTestCreateIndex {
 
     @After
     public void teardown() throws Exception {
-        boolean indexDelete = esClient.deleteIndex(indexName, defaultLogger);
+        esClient.deleteIndex(indexName, defaultLogger);
     }
 
 }
